1373b3c8c5e9be1f67b4f8e612e4fa4e9ea3db1e
Krzysztof Hadzicki <krych14m@gmail.com>
2017-03-19 19:28:54 +0100

    AndroidAnnotations included :D

--- app/src/main/java/pl/krych14m/ramki/wycennikram/RecentProductsActivity.java
+++ app/src/main/java/pl/krych14m/ramki/wycennikram/RecentProductsActivity.java
@@ -1,47 +1,37 @@
 package pl.krych14m.ramki.wycennikram;
 
-import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsItem;
+import org.androidannotations.annotations.OptionsMenu;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_recent_products)
+@OptionsMenu(R.menu.menu_recent_products)
 public class RecentProductsActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recent_products);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    @ViewById(R.id.toolbar)
+    Toolbar toolbar;
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    @ViewById(R.id.fab)
+    FloatingActionButton fab;
+
+    @Click(R.id.fab)
+    void doFab() {
+        Snackbar.make(fab, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_recent_products, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    @OptionsItem(R.id.action_settings)
+    void doSettings() {
+        Snackbar.make(fab, "Settings", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show();
     }
 }
