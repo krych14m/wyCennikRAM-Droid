bb0a1427059815970b9bfde48c2ef9fb497aa7bb
Krzysztof Hadzicki <krych14m@gmail.com>
2017-03-20 22:52:21 +0100

    TwoDimensionalProduct: Lombok style

--- app/src/main/java/pl/krych14m/ramki/wycennikram/api/products/TwoDimensionalProduct.java
+++ app/src/main/java/pl/krych14m/ramki/wycennikram/api/products/TwoDimensionalProduct.java
@@ -1,21 +1,11 @@
 package pl.krych14m.ramki.wycennikram.api.products;
 
+import lombok.Data;
+
+@Data
 public abstract class TwoDimensionalProduct implements Product {
 
     private final double x;
     private final double y;
-
-    public TwoDimensionalProduct(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
 
 }
