22501457bf5ae05df987db13a5f0130f8571ea86
Krzysztof Hadzicki <krych14m@gmail.com>
2017-04-08 16:08:10 +0200

    raw price resources added

--- app/src/main/java/pl/krych14m/ramki/wycennikram/ramki/RamkiCalculator.java
+++ app/src/main/java/pl/krych14m/ramki/wycennikram/ramki/RamkiCalculator.java
@@ -1,24 +1,34 @@
 package pl.krych14m.ramki.wycennikram.ramki;
 
+import android.content.Context;
+
 import org.androidannotations.annotations.EBean;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import pl.krych14m.ramki.wycennikram.R;
 import pl.krych14m.ramki.wycennikram.api.calculators.Calculator;
 import pl.krych14m.ramki.wycennikram.api.calculators.CalculatorException;
 import pl.krych14m.ramki.wycennikram.api.calculators.MultiProductCalculator;
 import pl.krych14m.ramki.wycennikram.api.products.Product;
 import pl.krych14m.ramki.wycennikram.ramki.calculators.FrameCalculator;
 import pl.krych14m.ramki.wycennikram.ramki.priceproviders.AccessoryParametersProvider;
+import pl.krych14m.ramki.wycennikram.ramki.priceproviders.AccessoryParametersProviderImpl;
 import pl.krych14m.ramki.wycennikram.ramki.priceproviders.ProfilePriceProvider;
+import pl.krych14m.ramki.wycennikram.ramki.priceproviders.ProfilePriceProviderImpl;
 
 @EBean(scope = EBean.Scope.Singleton)
 public class RamkiCalculator implements Calculator {
 
     private final MultiProductCalculator multiCalculator;
+    private final Context context;
 
-    public RamkiCalculator() {
+    public RamkiCalculator(Context context) {
+        this.context = context;
         ProfilePriceProvider profilePriceProvider = prepareProfilePriceProvider();
         AccessoryParametersProvider accessoryParametersProvider = prepareAccessoryParametersProvider();
         List<Calculator> calculators = prepareCalculators(profilePriceProvider, accessoryParametersProvider);
@@ -36,16 +46,31 @@ public class RamkiCalculator implements Calculator {
     }
 
     private List<Calculator> prepareCalculators(ProfilePriceProvider profilePriceProvider, AccessoryParametersProvider accessoryParametersProvider) {
+        if (profilePriceProvider == null || accessoryParametersProvider == null) {
+            //TODO info box
+            //TODO app terminate
+            return Collections.emptyList();
+        }
         List<Calculator> calculators = new ArrayList<>();
         calculators.add(new FrameCalculator(profilePriceProvider, accessoryParametersProvider));
         return calculators;
     }
 
     private ProfilePriceProvider prepareProfilePriceProvider() {
-        throw new Error("not yet implemented");
+        InputStream inputStream = context.getResources().openRawResource(R.raw.cennik_ramek);
+        try {
+            return new ProfilePriceProviderImpl(inputStream);
+        } catch (IOException e) {
+            return null;
+        }
     }
 
     private AccessoryParametersProvider prepareAccessoryParametersProvider() {
-        throw new Error("not yet implemented");
+        InputStream inputStream = context.getResources().openRawResource(R.raw.cennik_akcesoriow);
+        try {
+            return new AccessoryParametersProviderImpl(inputStream);
+        } catch (IOException e) {
+            return null;
+        }
     }
 }
